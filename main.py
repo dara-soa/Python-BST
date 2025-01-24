1ffa2eae089374c2558be3afeabe36369fa61fa4
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2025-01-23 21:45:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 class TreeNode:
+
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
+
+
 class BinarySearchTree:
+
     def __init__(self):
-        pass
+        self.root = None
+
+    def _insert(self, node, key):
+        if node is None:
+            return TreeNode(key)
+
+        if key < node.key:
+            pass
